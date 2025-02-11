445588d07a69428667410499c79b2353b9dd0277
smartcat5 <romanlill9227@gmail.com>
2025-02-11 04:56:19 +0200

    remote redundancy

--- smartcat5.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch --env-filter '
-CORRECT_NAME="smartcat5"
-CORRECT_EMAIL="romanlill9227@gmail.com"
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-' --tag-name-filter cat -- --branches --tags
